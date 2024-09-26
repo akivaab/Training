87c40320602137860ba39cb27d18846066f56233
akivaab <amabramo18@gmail.com>
2024-09-26 15:55:06 +0300

    js basics

--- 2-JavaScript/task-list.js
+++ 2-JavaScript/task-list.js
@@ -1,20 +1,71 @@
-let taskList = document.getElementById("task-list")
+let todoBtn = document.getElementById("todo-btn")
+let completedBtn = document.getElementById("completed-btn")
+let todoTaskList = document.getElementById("todo-task-list")
+let completedTaskList = document.getElementById("completed-task-list")
+let addTaskBtn = document.getElementById("add-task-btn")
+let addTaskDescInput = document.getElementById("add-task-desc-input")
 
-function init() {
-    let todoTaskList = localStorage.getItem("todo-task-list") ? JSON.parse(localStorage.getItem("todo-task-list")) : [];
-    let completedTaskList = localStorage.getItem("completed-task-list") ? JSON.parse(localStorage.getItem("completed-task-list")) : [];
+let todoTaskDescriptions = localStorage.getItem("todo-task-descriptions") ? JSON.parse(localStorage.getItem("todo-task-descriptions")) : [];
+let completedTaskDescriptions = localStorage.getItem("completed-task-descriptions") ? JSON.parse(localStorage.getItem("completed-task-descriptions")) : [];
 
-    let todoListHTML = ""
-    for (todoTaskDesc of todoTaskList) {
-        todoListHTML += `<li class="task todo">
-                            <div class="task-drag">drag</div>
-                            <p class="task-desc">${todoTaskDesc}</p>
-                            <button class="task-completed-btn">completed</button>
-                            <button class="task-delete-btn">delete</button>
-                        </li>`
+// initialize todoTaskList
+let todoListHTML = ""
+for (const todoTaskDesc of todoTaskDescriptions) {
+    todoListHTML += `<li class="todo-task">
+                        <div class="task-drag">drag</div>
+                        <p class="task-desc">${todoTaskDesc}</p>
+                        <button class="task-completed-btn">completed</button>
+                        <button class="task-delete-btn">delete</button>
+                    </li>`
+}
+todoTaskList.innerHTML = todoListHTML
+
+// initialize completeTaskList
+let completedListHTML = ""
+for (const completedTaskDesc of completedTaskDescriptions) {
+    completedListHTML +=    `<li class="completed-task"></li>
+                                <div class="task-drag">drag</div>
+                                <p class="task-desc">${completedTaskDesc}</p>
+                                <button class="task-delete-btn">delete</button>
+                            </li>`
+}
+completedTaskList.innerHTML = completedListHTML
+
+// add event listeners to buttons
+todoBtn.addEventListener("click", () => {
+    todoBtn.style.display = "none"
+    completedBtn.style.display = "block"
+})
+completedBtn.addEventListener("click", () => {
+    completedBtn.style.display = "none"
+    todoBtn.style.display = "block"
+})
+addTaskBtn.addEventListener("click", () => {
+    let addTaskDesc = addTaskDescInput.value
+    if (addTaskDesc != "") {
+        addTaskDescInput.value = ""
+        todoTaskList.innerHTML +=   `<li class="todo-task">
+                                        <div class="task-drag">drag</div>
+                                        <p class="task-desc">${addTaskDesc}</p>
+                                        <button class="task-completed-btn">completed</button>
+                                        <button class="task-delete-btn">delete</button>
+                                    </li>`
     }
-    taskList.innerHTML = todoListHTML
-    //do same for invisible completeTaskList
+})
 
-    // add event listeners to buttons
+for (const deleteBtn of document.querySelectorAll(".task-delete-btn")) {
+    deleteBtn.addEventListener("click", () => {
+        deleteBtn.closest("li").remove()
+    })
+}
+for (const completedBtn of document.querySelectorAll(".task-completed-btn")) {
+    completedBtn.addEventListener("click", () => {
+        let desc = completedBtn.previousElementSibling.textContent
+        completedBtn.closest("li").remove()
+        completedTaskList.innerHTML +=  `<li class="completed-task"></li>
+                                            <div class="task-drag">drag</div>
+                                            <p class="task-desc">${desc}</p>
+                                            <button class="task-delete-btn">delete</button>
+                                        </li>`
+    })
 }
